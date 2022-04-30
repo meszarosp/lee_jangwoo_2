808f1208dc8697220a2c71ce157c92d40023326a
meszi0420 <57116193+meszi0420@users.noreply.github.com>
2022-04-30 19:59:51 +0200

    Update Control.java

--- src/Control.java
+++ src/Control.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.*;
